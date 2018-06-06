4179bf1db16635e6ab5f555341a06272aa78a686
miguelmafra <miguelml159357@gmail.com>
2018-06-05 21:59:52 -0300

    Arrumando Login no navbar

---
+++ eLibrary/Modelo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Modelo.Startup))]
+namespace Modelo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
