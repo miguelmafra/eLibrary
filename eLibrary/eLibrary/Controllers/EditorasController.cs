0a980fe774315bba8d5f892c952d45e0faf71fd5
miguelmafra <miguelml159357@gmail.com>
2018-06-10 03:43:09 -0300

    Criando Controller e View Editoras

---
+++ eLibrary/eLibrary/Controllers/EditorasController.cs
@@ -0,0 +1,121 @@
+﻿using eLibrary.Models;
+using eLibrary.Models.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace eLibrary.Controllers
+{
+    public class EditorasController : Controller
+    {
+        // GET: Editoras
+        public ActionResult Index()
+        {
+
+            MeuContexto contexto = new MeuContexto();
+
+            List<Editora> editoras = contexto.Editoras.ToList();
+
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Editora editora)
+        {
+            if (ModelState.IsValid)
+            {
+                MeuContexto contexto = new MeuContexto();
+                contexto.Editoras.Add(editora);
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(editora);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Editora edi = contexto.Editoras.Find(id);
+
+            if (edi == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(edi);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Editora edi = contexto.Editoras.Find(id);
+
+            if (edi == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(edi);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Editora edi)
+        {
+            if (ModelState.IsValid)
+            {
+                MeuContexto contexto = new MeuContexto();
+                contexto.Entry(edi).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(edi);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Editora edi = contexto.Editoras.Find(id);
+            if (edi == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(edi);
+
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MeuContexto contexto = new MeuContexto();
+            Editora edi = contexto.Editoras.Find(id);
+
+            contexto.Editoras.Remove(edi);
+            contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
