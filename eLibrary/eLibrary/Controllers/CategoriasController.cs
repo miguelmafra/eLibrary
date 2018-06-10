5785118c06426aafb0a30c51df0111b647c1092c
miguelmafra <miguelml159357@gmail.com>
2018-06-10 03:27:27 -0300

    CategoriasController e View de Categorias prontos.

---
+++ eLibrary/eLibrary/Controllers/CategoriasController.cs
@@ -0,0 +1,123 @@
+﻿using eLibrary.Models;
+using eLibrary.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eLibrary.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+
+            MeuContexto contexto = new MeuContexto();
+
+            List<Categoria> categorias = contexto.Categorias.ToList();
+
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                MeuContexto contexto = new MeuContexto();
+                contexto.Categorias.Add(categoria);
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Categoria cat = contexto.Categorias.Find(id);
+
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(cat);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Categoria cat = contexto.Categorias.Find(id);
+
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(cat);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria cat)
+        {
+            if (ModelState.IsValid)
+            {
+                MeuContexto contexto = new MeuContexto();
+                contexto.Entry(cat).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Categoria cat = contexto.Categorias.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(cat);
+
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MeuContexto contexto = new MeuContexto();
+            Categoria cat = contexto.Categorias.Find(id);
+
+            contexto.Categorias.Remove(cat);
+            contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ eLibrary/eLibrary/Models/DAL/MeuContexto.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace eLibrary.Models.DAL
+{
+    public class MeuContexto : DbContext 
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+            Database.SetInitializer<MeuContexto>(new DropCreateDatabaseIfModelChanges<MeuContexto>());
+
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Editora> Editoras { get; set; }
+        public DbSet<Assunto> Assuntos { get; set; }
+        
+
+    }
+}
