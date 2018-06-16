fcd19ec75f784f70d4ff31350abf71c9c87847d3
miguelmafra <miguelml159357@gmail.com>
2018-06-16 14:43:01 -0300

    Arrumando model Livro (Assunto)

--- eLibrary/eLibrary/Models/Livro.cs
+++ eLibrary/eLibrary/Models/Livro.cs
@@ -21,7 +21,8 @@ namespace eLibrary.Models
         public Categoria CategoriaID { get; set; }
         public int _EditoraID { get; set; }
         public Editora EditoraID { get; set; }
-        public int MyProperty { get; set; }
+        public int _AssuntoID { get; set; }
+        public Assunto AssuntoID { get; set; }
         //public string Assunto { get; set; }
         //public string Editora { get; set; } (Herança)
         //public string Categoria { get; set; } (Herança)
