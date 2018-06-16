f0b313c0966e335e6ad6d1faca200c0a998bbfea
miguelmafra <miguelml159357@gmail.com>
2018-06-16 14:32:19 -0300

    Criação LivroViewModel

---
+++ eLibrary/eLibrary/Models/LivroViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class LivroViewModel
+    {
+        public LivroViewModel(List<Livro> livros, List<Categoria> categorias, List<Editora> editoras, List<Assunto> assuntos)
+        {
+            this.Livros = livros;
+            this.Categorias = categorias;
+            this.Editoras = editoras;
+            this.Assuntos = assuntos;
+        }
+
+        public List<Livro> Livros { get; private set; }
+        public List<Categoria> Categorias { get; private set; }
+        public List<Editora> Editoras { get; private set; }
+        public List<Assunto> Assuntos { get; private set; }
+    }
+}
