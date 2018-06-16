c2dfd34e94bd87c7593f912e439f411b2cb3f39a
miguelmafra <miguelml159357@gmail.com>
2018-06-16 15:03:42 -0300

    Arrumando model livro (virtual)

--- eLibrary/eLibrary/Models/Livro.cs
+++ eLibrary/eLibrary/Models/Livro.cs
@@ -13,17 +13,17 @@ namespace eLibrary.Models
         public string Titulo { get; set; }
         public int ISBN { get; set; }
         public int Paginas { get; set; }
-        public int Edicao { get; set; }  
+        public int Edicao { get; set; }
         public string Autor { get; set; }
         public int Ano { get; set; }
         public string Origem { get; set; }
         public string Idioma { get; set; }
-        public int _CategoriaID { get; set; }
-        public Categoria CategoriaID { get; set; }
-        public int _EditoraID { get; set; }
-        public Editora EditoraID { get; set; }
-        public int _AssuntoID { get; set; }
-        public Assunto AssuntoID { get; set; }
+        public int CategoriaID { get; set; }
+        public virtual Categoria _Categoria { get; set; }
+        public int EditoraID { get; set; }
+        public virtual Editora _Editora { get; set; }
+        public int AssuntoID { get; set; }
+        public virtual Assunto _Assunto { get; set; }
         //public string Assunto { get; set; }
         //public string Editora { get; set; } (Herança)
         //public string Categoria { get; set; } (Herança)
