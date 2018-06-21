41d46d08db0dc03698d11ba4a6266a8e28f9541f
miguelmafra <miguelml159357@gmail.com>
2018-06-21 20:32:21 -0300

    Criação EditViewModel

---
+++ eLibrary/eLibrary/Models/EditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class EditViewModel
+    {
+        public EditViewModel(Livro livro, List<Categoria> categorias, List<Editora> editoras, List<Assunto> assuntos)
+        {
+            this.Livro = livro;
+            this.Categorias = categorias;
+            this.Editoras = editoras;
+            this.Assuntos = assuntos;
+        }
+
+        public Livro Livro { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma categoria da lista")]
+        public List<Categoria> Categorias { get; set; }
+        [Required(ErrorMessage = "Selecione uma editora da lista")]
+        public List<Editora> Editoras { get; set; }
+        [Required(ErrorMessage = "Selecione um assunto da lista")]
+        public List<Assunto> Assuntos { get; set; }
+    }
+}
