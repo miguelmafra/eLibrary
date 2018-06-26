041a69b8a87dbe44d1bd9c39e93b6048ffd0f4bf
miguelmafra <miguelml159357@gmail.com>
2018-06-25 23:19:02 -0300

    Criação de model Reseva

---
+++ eLibrary/eLibrary/Models/Reserva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int ReservaID { get; set; }
+        public int LivroID { get; set; }
+        public virtual Livro _Livro { get; set; }
+        public string UserID { get; set; }
+        public virtual ApplicationUser _ApplicationUser { get; set; }
+    }
+}
