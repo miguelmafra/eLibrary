b8e501b4cbfc45203b2b0f10f1d3a79bd4e6227f
miguelmafra <miguelml159357@gmail.com>
2018-06-28 16:12:03 -0300

    Criação ReservasViewModel

---
+++ eLibrary/eLibrary/Models/ReservasViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class ReservasViewModel
+    {
+
+        public ReservasViewModel(List<Livro> livros, List<Reserva> reservas)
+        {
+            this.Livros = livros;
+            this.Reservas = reservas;
+        }
+
+        public List<Livro> Livros { get; set; }
+        public List<Reserva> Reservas { get; set; }
+    }
+}
+}
